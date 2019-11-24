3e1054ebc04a1e66f7a1d4653530bcef2ea9529c
waver <jokerwaver@gmail.com>
2019-11-24 22:44:06 +0800

    add SQL File

--- src/main/resources/sql/dialogue.sql
+++ src/main/resources/sql/dialogue.sql
@@ -11,23 +11,23 @@
  Target Server Version : 80017
  File Encoding         : 65001
 
- Date: 24/11/2019 22:32:43
+ Date: 24/11/2019 22:42:06
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for dia_user
+-- Table structure for dialog
 -- ----------------------------
-DROP TABLE IF EXISTS `dia_user`;
-CREATE TABLE `dia_user`  (
-  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户ID',
-  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
-  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
-  `statue` int(1) NOT NULL DEFAULT 1 COMMENT '用户状态 1: 正常 0:禁止登录  -1: 已删除',
-  `create_time` bigint(13) NOT NULL COMMENT '创建时间',
-  `update_time` bigint(13) NOT NULL COMMENT '更新时间',
+DROP TABLE IF EXISTS `dialog`;
+CREATE TABLE `dialog`  (
+  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `type` int(3) NOT NULL COMMENT '类型: 0: 顶级目录 1: 子目录 2: 对话文章',
+  `parent_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '父节点ID  -1: 顶级父节点',
+  `creat_date` bigint(13) NOT NULL COMMENT '创建时间',
+  `update_date` bigint(13) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
@@ -61,16 +61,17 @@ CREATE TABLE `dialog_sentence`  (
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Table structure for dialogue
+-- Table structure for dialog_user
 -- ----------------------------
-DROP TABLE IF EXISTS `dialogue`;
-CREATE TABLE `dialogue`  (
-  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `type` int(3) NOT NULL COMMENT '类型: 0: 顶级目录 1: 子目录 2: 对话文章',
-  `parent_id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '父节点ID  -1: 顶级父节点',
-  `creat_date` bigint(13) NOT NULL COMMENT '创建时间',
-  `update_date` bigint(13) NOT NULL COMMENT '更新时间',
+DROP TABLE IF EXISTS `dialog_user`;
+CREATE TABLE `dialog_user`  (
+  `id` varchar(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户ID',
+  `user_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '用户名称',
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '密码',
+  `statue` int(1) NOT NULL DEFAULT 1 COMMENT '用户状态 1: 正常 0:禁止登录  -1: 已删除',
+  `grade` int(3) NOT NULL COMMENT '年级',
+  `create_time` bigint(13) NOT NULL COMMENT '创建时间',
+  `update_time` bigint(13) NOT NULL COMMENT '更新时间',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
